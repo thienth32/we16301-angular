bf12bedf6c746529650b299f68cd775a083a73a4
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-03-11 13:12:39 +0700

    form tạo hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'we16301-angular';
+  formHero: any = {
+    code: "",
+    name: "",
+    avatar: "",
+    attack: 0,
+    gender: "Nam"
+  }
+
+  heroes: Array<any> = [
+    {
+      code: "sgk",
+      name: "Songoku",
+      avatar: "https://i.pinimg.com/736x/d7/bd/23/d7bd2389fd03889edc309504c3377409.jpg",
+      attack: 50,
+      gender: "Nam"
+    },
+    {
+      code: "wof",
+      name: "Wolverine",
+      avatar: "https://fandom.vn/wp-content/uploads/2019/06/mcu-wolverine-1.jpg",
+      attack: 56,
+      gender: "Nam"
+    }
+  ];
+
+  removeHero(hero: any){
+    this.heroes = this.heroes.filter(item => item.code != hero.code);
+  }
 }
